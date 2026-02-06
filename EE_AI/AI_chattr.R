84de83100aef24968e8ea3bbedb542bf81301c00
bioz <bioinfo.gao@gmail.com>
2026-02-05 20:52:24 -0500

    AI2

--- EE_AI/AI_chattr.R
+++ EE_AI/AI_chattr.R
@@ -1,2 +1,16 @@
+# https://www.youtube.com/watch?v=dxBR191Vc8A
 installed.packages("chattr")
 library(chattr)
+
+##  Setting Up chattr in RStudio: A Complete Guide
+# https://rstudio-pubs-static.s3.amazonaws.com/1360735_64ad9b3ba74d4f60bff2f36e94e2ab35.html
+# https://cloud.r-project.org/web/packages/chattr/readme/README.html
+
+## Get an Open AI PI key
+# 1) create an account
+# 2) setup payment method with limit
+# 3) create an API key
+
+
+Sys.getenv("OPENAI_API_KEY")
+chattr_app()
