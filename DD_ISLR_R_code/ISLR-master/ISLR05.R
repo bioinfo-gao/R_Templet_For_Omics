bd6ce1b62be16d766d572441743054f815ebb711
bioz <bioinfo.gao@gmail.com>
2026-02-08 19:55:01 -0500

    ISL

--- DD_ISLR_R_code/ISLR-master/ISLR05.R
+++ DD_ISLR_R_code/ISLR-master/ISLR05.R
@@ -18,6 +18,7 @@ mean((mpg - predict(lm.fit,Auto))[-train]^2)
 
 lm.fit2 <- lm(mpg ~ poly(horsepower, 2), data = Auto, subset = train)
 mean((mpg - predict(lm.fit2,Auto))[-train]^2)
+
 lm.fit3 <- lm(mpg ~ poly(horsepower, 3), data = Auto, subset = train)
 mean((mpg - predict(lm.fit3, Auto))[-train]^2)
 
